4f16ccc6c476177cb119367d9da2e8631f9cbb8e
Fuen Sansano <fuensansano@gmail.com>
2023-10-17 23:40:45 +0200

    refactor production code

--- src/mastermind.rb
+++ src/mastermind.rb
@@ -4,13 +4,21 @@ class Mastermind
   def evaluate(win_combination, guess_combination)
     return [win_combination.length, 0] if win_combination == guess_combination
 
-    count_right_color_and_right_position = win_combination.zip(guess_combination).count { |win_element, guess_element| win_element == guess_element }
-
-    set_win_combination = Set.new(win_combination)
-    set_guess_combination = Set.new(guess_combination)
-
-    count_right_color_distinct_position = (set_win_combination & set_guess_combination).count
+    count_right_color_and_right_position = extract_number_rigth_colors_and_rigth_position(guess_combination, win_combination)
+    count_right_color_distinct_position = extract_number_rigth_color_in_different_position(win_combination, guess_combination)
 
     [count_right_color_and_right_position,count_right_color_distinct_position]
+  end
+
+  private
+
+  def extract_number_rigth_color_in_different_position(win_combination, guess_combination)
+    set_win_combination = Set.new(win_combination)
+    set_guess_combination = Set.new(guess_combination)
+    (set_win_combination & set_guess_combination).count
+  end
+
+  def extract_number_rigth_colors_and_rigth_position(guess_combination, win_combination)
+    win_combination.zip(guess_combination).count { |win_element, guess_element| win_element == guess_element }
   end
 end
