921bed431b1c818b7c7d04d5ef5851c5077598a2
Fuen Sansano <fuensansano@gmail.com>
2023-10-17 23:09:53 +0200

    REFACTOR: simplify when the win_combination is equal to the guess_combination

--- src/mastermind.rb
+++ src/mastermind.rb
@@ -2,9 +2,7 @@
 
 class Mastermind
   def evaluate(win_combination, guess_combination)
-    return [2,0] if win_combination[0] == 'blue' && win_combination[1] == 'red'
-    return [2,0] if win_combination[0] == 'red' && win_combination[1] == 'blue'
-    return [0,0] if win_combination[0] != guess_combination[0]
-    [1,0]
+    return [win_combination.length, 0] if win_combination == guess_combination
+    [0,0]
   end
 end
