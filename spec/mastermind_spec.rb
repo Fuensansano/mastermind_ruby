3f7d713290d1bb4b7577c26092764d4d602a82ba
Fuen Sansano <fuensansano@gmail.com>
2023-10-17 22:48:49 +0200

    REFACTOR: extract mastermind variable

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,21 +1,17 @@
 require_relative '../src/mastermind'
 
 describe Mastermind do
-  it 'should return [1,0] when guesser guest the combination' do
-    mastermind = Mastermind.new
+  let(:mastermind) { Mastermind.new }
 
+  it 'should return [1,0] when guesser guest the combination' do
     expect(mastermind.evaluate(['blue'],['blue'])).to eq([1,0])
   end
 
   it 'should return [0,0] when guesser fails to guess the combination' do
-    mastermind = Mastermind.new
-
     expect(mastermind.evaluate(['red'],['blue'])).to eq([0,0])
   end
 
   it 'should return [0,0] when guesser fails to guess the combination with yellow' do
-    mastermind = Mastermind.new
-
     expect(mastermind.evaluate(['yellow'],['red'])).to eq([0,0])
   end
 end
