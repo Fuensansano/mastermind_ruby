7c8d411eed92de3fb0a7e26b7e1ce3d2bfd815ab
Fuen Sansano <fuensansano@gmail.com>
2023-10-17 22:27:08 +0200

    GREEN: should return [0,0] when guesser fails to guess the combination

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -6,4 +6,10 @@ describe Mastermind do
 
     expect(mastermind.evaluate(['blue'],['blue'])).to eq([1,0])
   end
+
+  it 'should return [0,0] when guesser fails to guess the combination' do
+    mastermind = Mastermind.new
+
+    expect(mastermind.evaluate(['red'],['blue'])).to eq([0,0])
+  end
 end
--- src/mastermind.rb
+++ src/mastermind.rb
@@ -2,6 +2,7 @@
 
 class Mastermind
   def evaluate(win_combination, guess_combination)
+    return [0,0] if win_combination[0] == 'red'
     [1,0]
   end
 end
